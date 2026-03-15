6d9f693b8af00524534f5ef2fd73a41f54d109e0
Mayuri-Tanpure <mayuritanpure3@gmail.com>
2026-03-15 22:52:38 +0530

    Create primenumbers.php

---
+++ primenumbers.php
@@ -0,0 +1,22 @@
+<?php
+
+function checkPrime($num){
+
+    $flag = 0;
+
+    for($i = 2; $i <= $num/2; $i++){
+        if($num % $i == 0){
+            $flag = 1;
+            break;
+        }
+    }
+
+    if($flag == 0)
+        echo "$num is Prime";
+    else
+        echo "$num is Not Prime";
+}
+
+checkPrime(7);
+
+?>
