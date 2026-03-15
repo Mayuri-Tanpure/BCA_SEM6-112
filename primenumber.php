f0dc4006d79f4c74ae7a6906c478b620a075969c
Mayuri-Tanpure <mayuritanpure3@gmail.com>
2026-03-15 22:42:41 +0530

    Create primenumber.php

---
+++ primenumber.php
@@ -0,0 +1,16 @@
+<?php
+$num = 7;
+$flag = 0;
+
+for($i = 2; $i <= $num/2; $i++){
+    if($num % $i == 0){
+        $flag = 1;
+        break;
+    }
+}
+
+if($flag == 0)
+echo "$num is Prime";
+else
+echo "$num is Not Prime";
+?>
