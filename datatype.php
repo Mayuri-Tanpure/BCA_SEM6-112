4f79b14590cbb8e9e31b7f95ffc731ce8c746b07
Mayuri-Tanpure <mayuritanpure3@gmail.com>
2026-03-15 22:34:56 +0530

    Create datatype.php

---
+++ datatype.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Manu";      // String
+$age = 21;           // Integer
+$height = 5.6;       // Float
+$isStudent = true;   // Boolean
+
+echo "Name: $name <br>";
+echo "Age: $age <br>";
+echo "Height: $height <br>";
+echo "Student: $isStudent";
+?>
